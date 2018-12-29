9a0df403a3a001e0c3afb77c23e226542e2ff5fb
JobQiu <congminqi@gmail.com>
2018-12-28 21:18:48 -0600

    ...

--- Congmin Qiu/draft.py
+++ Congmin Qiu/draft.py
@@ -1,53 +1,49 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+import re
 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution:
-    def buildTree(self, preorder, inorder):
-        """
-        :type preorder: List[int]
-        :type inorder: List[int]
-        :rtype: TreeNode
-        """
-        if not preorder:
-            return None
-
-        i = 1
-        j = 0
-        root = TreeNode(preorder[0])
-        s = [root]
-        n = len(preorder)
-        while i < n:
-            # check if top has more left nodes
-            if inorder[j] != s[-1].val:
-                node = TreeNode(preorder[i])
-                s[-1].left = node
-                s.append(node)
-                i += 1
-            else:
-                # while s and preorder[i] != inorder[j]:
-                while s and s[-1].val == inorder[j]:
-                    parent = s.pop()
-                    j += 1
-
-                node = TreeNode(preorder[i])
-                parent.right = node
-                s.append(node)
-                i += 1
-
-        return root
+def deal_with_contraction(text):
+    specials = ["’", "‘", "´", "`"]
+    for s in specials:
+        text = text.replace(s, "'")
+    text = ' '.join([contraction_mapping[t] if t in contraction_mapping else t for t in text.split(" ")])
+    return text
 
 
-s = Solution()
-res = s.buildTree([], [])
-print("")
+contraction_mapping = {"ain't": "is not", "aren't": "are not", "can't": "cannot", "'cause": "because",
+                       "could've": "could have", "couldn't": "could not", "didn't": "did not", "doesn't": "does not",
+                       "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not",
+                       "he'd": "he would", "he'll": "he will", "he's": "he is", "how'd": "how did",
+                       "how'd'y": "how do you", "how'll": "how will", "how's": "how is", "I'd": "I would",
+                       "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have", "I'm": "I am",
+                       "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",
+                       "i'll've": "i will have", "i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would",
+                       "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have", "it's": "it is",
+                       "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have",
+                       "mightn't": "might not", "mightn't've": "might not have", "must've": "must have",
+                       "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not",
+                       "needn't've": "need not have", "o'clock": "of the clock", "oughtn't": "ought not",
+                       "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not",
+                       "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have",
+                       "she'll": "she will", "she'll've": "she will have", "she's": "she is",
+                       "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have",
+                       "so've": "so have", "so's": "so as", "this's": "this is", "that'd": "that would",
+                       "that'd've": "that would have", "that's": "that is", "there'd": "there would",
+                       "there'd've": "there would have", "there's": "there is", "here's": "here is",
+                       "they'd": "they would", "they'd've": "they would have", "they'll": "they will",
+                       "they'll've": "they will have", "they're": "they are", "they've": "they have",
+                       "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have",
+                       "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have",
+                       "weren't": "were not", "what'll": "what will", "what'll've": "what will have",
+                       "what're": "what are", "what's": "what is", "what've": "what have", "when's": "when is",
+                       "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have",
+                       "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have",
+                       "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not",
+                       "won't've": "will not have", "would've": "would have", "wouldn't": "would not",
+                       "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would",
+                       "y'all'd've": "you all would have", "y'all're": "you all are", "y'all've": "you all have",
+                       "you'd": "you would", "you'd've": "you would have", "you'll": "you will",
+                       "you'll've": "you will have", "you're": "you are", "you've": "you have"}
+
+origin = "this colour seem didn't not thertre"
+res = deal_with_contraction(origin)
+print(res)
