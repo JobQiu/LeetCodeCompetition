79cd03a9b93791f290773dc8e59f7f1b53929b86
JobQiu <congminqi@gmail.com>
2019-02-03 06:33:18 -0600

    ...

--- Congmin Qiu/316.py
+++ Congmin Qiu/316.py
@@ -1,19 +1,6 @@
-from collections import Counter
 
 class Solution:
     def removeDuplicateLetters(self, s):
-        # 1. firstly decide the first char
+        """
 
-        c = Counter(s)
-        pos = 0
-
-        for i in range(len(s)):
-            if s[i] < s[pos]:
-                pos = i
-            c[s[i]] -= 1
-            if c[s[i]] == 0:
-                break
-
-        return "" if len(s) == 0 else s[pos] + self.removeDuplicateLetters(s[pos+1:].replace(s[pos], ""))
-
-        pass
+        """
